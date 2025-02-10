42f74be26dd5f8ce532ee8563b85d2cffe8c44a0
drh-14 <darrenh1134@gmail.com>
2025-02-10 12:29:26 -0500

    finished landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
-import { Box, Button } from '@mui/material';
+'use client'
+import {Button} from '@mui/material';
+import {useRouter} from 'next/navigation';
 export default function Home() {
+  const router = useRouter();
   return (
-    <div className = 'flex flex-col gap-8 items-center'>
-      <div className = 'flex flex-col gap-16 mt-48 items-center justify-center'>
-        <h1 className = 'text-center text-5xl font-mono'>Introducing SemesterPal</h1>
-        <h2 className = 'text-center text-4xl'>The ultimate tool to manage your semester.</h2>
-        <h3 className = 'text-center text-3xl'>Track final grades in all of your classes at the click of a  button.</h3>
-        <Button size = "large" variant = 'contained'>Get Started</Button>
+    <div className = 'flex flex-col gap-40'>
+    <div className = 'flex gap-4 mt-4 justify-end mr-4'>
+        <Button onClick = {() => router.push('/login')}>Login</Button>
+        <Button onClick = {() => router.push('/signUp')}>Sign Up</Button>
       </div>
-
+    <div className = 'flex flex-col gap-8 items-center'>
+      <div className = 'flex flex-col gap-16 items-center justify-center'>
+        <h3 className = 'text-4xl'>Introducing SemesterPal</h3>
+        <h3 className = 'text-3xl'>The ultimate tool to manage your semester.</h3>
+        <h3 className = 'text-3xl'>Track final grades in all of your classes at the click of a  button.</h3>
+        <Button variant = 'contained' size = 'large' onClick = {() => router.push('/signUp')}>Get Started</Button>
+      </div>
+    </div>
     </div>
   );
 }
